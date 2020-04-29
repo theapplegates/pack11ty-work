6260df2dea80cfe7aaf7eabb44fab536b8d54362
Paul <theapplegates@users.noreply.github.com>
2020-04-29 19:43:24 -0400

    fix cloudinary url

--- src/_11ty/shortcodes/video.js
+++ src/_11ty/shortcodes/video.js
@@ -6,7 +6,7 @@ module.exports = {
     return `<figure class="video"><iframe width="784" height="441" src="https://player.vimeo.com/video/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></figure>`;
   },
   giphy: (id) => {
-    const cloudinaryPrefix = 'https://res.cloudinary.com/nho/image/fetch';
+    const cloudinaryPrefix = 'https://res.cloudinary.com/paulportfolio/image/fetch';
     const giphyImage = `https://media.giphy.com/media/${id}/giphy.gif`;
     webmSource = `<source src="${cloudinaryPrefix}/f_webm/${giphyImage}" type="video/webm">`;
     mp4Source = `<source src="${cloudinaryPrefix}/f_mp4/${giphyImage}" type="video/mp4">`;
